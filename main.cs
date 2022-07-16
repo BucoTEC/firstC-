f74d9664648738c1933c4f60f82e9466abda15d8
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-16 10:19:27 +0200

    reconfigure main method

--- main.cs
+++ main.cs
@@ -2,6 +2,12 @@ using System;
 
 class Hello
 {
+
+    Hello(){
+
+        Console.WriteLine("Hello from the other side");
+    }
+
     static void Main(){
         Console.WriteLine("Hello");
     }
