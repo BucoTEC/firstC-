a11869c51eb1310fbc6ac37f7c9b9aced0870f5a
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-17 23:15:39 +0200

    reconfigure safet

--- Test.cs
+++ Test.cs
@@ -3,6 +3,8 @@ using System;
 namespace Test
 {
     class Safet{
+
+        public string[] imena = new string[] {"adnan", "advna", "arman"};
         public static void Hello(){
              Console.WriteLine("Hello from safet");
         }
