e94c9a7bf3d675bda0966ed019697cd90bc84d0b
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-26 21:09:32 +0200

    cleen up

--- Program.cs
+++ Program.cs
@@ -1,17 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Test;
-string adnan = "   pozdrav moje ime je adnan sta ima kod vas";
-
-var trimovan = adnan.Trim();
-string[] imena = {"Riad", "Advna"};
-Console.WriteLine(trimovan.Contains("adnan"));
-Safet.Hello();
-Safet.MyName();
-if(args.Length != 0){
-
-Console.WriteLine(args[0]);
-}
-foreach (var item in imena)
-{
-    Console.WriteLine(item);
-}
+Console.WriteLine("Hello, World!");
