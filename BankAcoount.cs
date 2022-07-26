90fa7cba330586b36e805782e2af3d7aea386edf
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-26 22:45:26 +0200

    add basic accoinut number

--- BankAcoount.cs
+++ BankAcoount.cs
@@ -3,10 +3,14 @@ public class BankAccount{
     public string Owner {get;set;}
     public decimal Balance {get; set;}
 
+    private static int Rand = 1234;
+
     public  BankAccount(string name, decimal initialBalance ){
         Owner = name;
         Balance = initialBalance;
+        Number = Rand.ToString();
     }
+
 
 
 
