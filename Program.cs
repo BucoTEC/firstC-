93bc310928cb2128e2ed9647e5ccf31271e5f5e5
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-26 18:50:26 +0200

    basic string manipulation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Test;
+string adnan = "Adnan";
 
+Console.WriteLine(adnan);
 Safet.Hello();
 Safet.MyName();
 Console.WriteLine("Hello, World!");
