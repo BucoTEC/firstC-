22d933e4c821b2fc24dc94f8087f76cdda1b3976
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-26 19:19:27 +0200

    implement basic foreac loop

--- Program.cs
+++ Program.cs
@@ -3,7 +3,15 @@ using Test;
 string adnan = "   pozdrav moje ime je adnan sta ima kod vas";
 
 var trimovan = adnan.Trim();
+string[] imena = {"Riad", "Advna"};
 Console.WriteLine(trimovan.Contains("adnan"));
 Safet.Hello();
 Safet.MyName();
+if(args.Length != 0){
+
 Console.WriteLine(args[0]);
+}
+foreach (var item in imena)
+{
+    Console.WriteLine(item);
+}
