51364dddd311693444e4faeefb8c925f5db11469
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-29 00:36:23 +0200

    setup basic static

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using StaticTest;
+
 var accout = new BankAccount("Adnan", 100000);
 
 Console.WriteLine(accout.Owner);
 Console.WriteLine("Hello, World!");
+Progg.Hell();
---
+++ StaticTest.cs
@@ -0,0 +1,9 @@
+namespace StaticTest{
+    public class Progg{
+       public static int Broj = 1;
+
+       public static void Hell(){
+        Console.WriteLine("hi from static");
+       }
+    }
+}
