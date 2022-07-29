0f0ca5f1af0c45e198fb43d4349f0855263aece1
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-29 12:41:39 +0200

    base cleen up

--- Program.cs
+++ Program.cs
@@ -1,12 +1,5 @@
-﻿using BankAccount;
-
-var newAccount = new Account("Adnan", 123);
-
-Console.WriteLine($"Hello from {newAccount.Owner} and his number is {newAccount.Number}");
-
-
-var secondAccount = new Account("Nejra", 333);
-
-Console.WriteLine(Account.test);
-
-Console.WriteLine($"Hello from {secondAccount.Owner} and his number is {secondAccount.Number}");
+﻿public class Program{
+    public static void Main(){
+        Console.WriteLine("Hello from c#");
+    }
+}
