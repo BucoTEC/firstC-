a77da004b972ca663dcbf07de2f00d0f1da719fb
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-29 09:22:01 +0200

    cleen up

--- Program.cs
+++ Program.cs
@@ -1,8 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-using StaticTest;
-
-var accout = new BankAccount("Adnan", 100000);
-
-Console.WriteLine(accout.Owner);
-Console.WriteLine("Hello, World!");
-Progg.Hell();
+﻿
--- StaticTest.cs
+++
@@ -1,9 +0,0 @@
-namespace StaticTest{
-    public class Progg{
-       public static int Broj = 1;
-
-       public static void Hell(){
-        Console.WriteLine("hi from static");
-       }
-    }
-}
