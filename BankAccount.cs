4dbdc662d3385cd1afad8c66b42e3c108a798cc9
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-29 09:25:54 +0200

    basic bankaccount class setup

---
+++ BankAccount.cs
@@ -0,0 +1,13 @@
+namespace BankAccount;
+    public class BankAccount
+    {
+        public string Number {get;}
+        public string Owner {get; set;}
+        public decimal Balance {get;}
+
+        public void MakeDeposid(decimal amount, DateTime date, string note){}
+
+        public void MakeWidthrawl(decimal amount, DateTime date, string note){}
+
+        
+    }
