452642177f57ba3f67e86589528a8e9f72089850
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-30 14:45:13 +0200

    rename controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace firstC_.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/character")]
     public class CharacterController : ControllerBase
     {
 
