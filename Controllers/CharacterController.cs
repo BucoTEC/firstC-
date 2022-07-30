bc79cfb9cfdc172bb69f74b5e31c4d91d674c073
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-30 12:33:10 +0200

    configure basic routing

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using firstC_.Models;
 namespace firstC_.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
         
