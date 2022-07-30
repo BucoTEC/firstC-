f6e538c70192216eff7740c7da513a193535ec3b
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-30 18:17:44 +0200

    add async to character controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -17,22 +17,22 @@ namespace firstC_.Controllers
             _characterService = characterService;
         }
         [HttpGet]
-        public ActionResult<List<Character>> Get(){
-            return Ok(_characterService.GetAllCharacter());
+        public async Task<ActionResult<List<Character>>> Get(){
+            return Ok(await _characterService.GetAllCharacter());
         }   
 
          [HttpGet("{id}")]
-        public ActionResult<Character> GetSingleCharacter(int id){
+        public async Task<ActionResult<Character>> GetSingleCharacter(int id){
 
-            return Ok(_characterService.GetCharacterById(id));
+            return Ok(await _characterService.GetCharacterById(id));
 
         }    
 
 
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character newCharacter){
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter){
 
-            return Ok(_characterService.AddCharacter(newCharacter));
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }   
     }
 }
