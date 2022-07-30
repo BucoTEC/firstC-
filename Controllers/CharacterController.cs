257f8c18b9e4389c9cb17a219c3e71f7c095f357
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-30 12:30:43 +0200

    fix using statments

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -2,10 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Mvc;
 namespace firstC_.Controllers
 {
-    public class CharacterController
+    public class CharacterController : ControllerBase
     {
         
     }
