eb2760cee22cf52bc4c8e548e6742bea71c36f34
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-30 12:34:03 +0200

    add global imports models

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using firstC_.Models;
 namespace firstC_.Controllers
 {
     [ApiController]
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+global using firstC_.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
