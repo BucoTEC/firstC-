aaaeca9bd0278b0cb3e8e27cd6cc11f114b93ab2
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-07-30 18:21:36 +0200

    config service response

---
+++ Models/ServiceRes.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firstC_.Models
+{
+    public class ServiceRes<T>
+    {
+        public T? Data { get; set; }
+        public bool Succes { get; set; } = true;
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
